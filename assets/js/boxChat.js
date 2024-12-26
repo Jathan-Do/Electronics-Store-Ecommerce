79f6981e0e3c1e2f3e45bb2f33d3166e5ae12c54
Jathan <nhatdangdo2502@gmail.com>
2024-12-26 13:56:44 +0700

    add chat botbot gpt demo

--- assets/js/boxChat.js
+++ assets/js/boxChat.js
@@ -2,20 +2,29 @@ $(document).ready(function () {
     const chatIcon = $("#chat-icon");
     const chatBox = $("#chat-box");
     const closeChat = $("#close-chat");
+    const resetChat = $("#reset-chat"); // Nút Reset
     const sendChat = $("#send-chat");
     const chatInput = $("#chat-input");
     const chatBody = $(".chat-body");
 
     // Hiển thị hộp chat khi bấm vào biểu tượng chat
     chatIcon.on("click", function () {
-        chatBox.show();
+        chatBox.stop().slideToggle(300, "swing");
         chatIcon.hide();
     });
 
     // Đóng hộp chat khi bấm vào nút đóng
-    closeChat.on("click", function () {
-        chatBox.hide();
+    closeChat.on("click", function (event) {
+        chatBox.stop().slideToggle(300, "swing");
         chatIcon.show();
+    });
+
+    // Đóng hộp chat khi nhấn bên ngoài
+    $(document).on("click", function (e) {
+        if (!$(e.target).closest(chatIcon).length && !$(e.target).closest(chatBox).length) {
+            chatBox.slideUp(300, "swing");
+            chatIcon.show();
+        }
     });
 
     // Gửi tin nhắn khi nhấn nút Gửi
@@ -28,28 +37,54 @@ $(document).ready(function () {
         }
     });
 
+    // Xử lý sự kiện khi bấm vào các nút câu hỏi mẫu
+    $(".question-button").on("click", function () {
+        const question = $(this).text();
+        const answer = $(this).data("answer");
+
+        addMessage("user", question);
+        showTypingEffect(() => {
+            addMessage("reply", `<img src="assets/img/logo/logo.svg" alt="" style="width: 100px" /> ${answer}`);
+        });
+
+        // Ẩn nút câu hỏi mẫu đã được bấm vào
+        $(".chat-questions").hide();
+    });
+
+    // Xử lý sự kiện khi nhấn nút Reset
+    resetChat.on("click", function () {
+        chatBody.find(".message").remove(); 
+        $(".chat-questions").show();
+    });
+
     function sendMessage() {
         const message = chatInput.val().trim();
         if (message) {
-            const messageElement = $("<div>").addClass("message user").text(message);
-            chatBody.append(messageElement);
-
-            // Hiển thị hiệu ứng "đang nhập"
-            const typingElement = $("<div>").addClass("message reply typing").html('<span class="dot">.</span><span class="dot">.</span><span class="dot">.</span>');
-            chatBody.append(typingElement);
-
-            chatInput.val(""); 
-            chatBody.scrollTop(chatBody.prop("scrollHeight"));
-
-            // Sau 2 giây, thay thế hiệu ứng "đang nhập" bằng tin nhắn phản hồi
-            setTimeout(function () {
-                typingElement.remove();
-
-                const replyElement = $("<div>").addClass("message reply").html('<img src="assets/img/logo/logo.svg" alt="" style="width: 100px" /> Xin vui lòng chờ trong giây lát sẽ có người hỗ trợ vấn đề của bạn. Cảm ơn bạn đã luôn tin tưởng Điện máy Đăng Nho 2 ạ. Chúc bạn một ngày tốt lành!');
-                chatBody.append(replyElement);
-
-                chatBody.scrollTop(chatBody.prop("scrollHeight"));
-            }, 2000); 
+            addMessage("user", message);
+            chatInput.val("");
+            showTypingEffect(() => {
+                addMessage("reply", '<img src="assets/img/logo/logo.svg" alt="" style="width: 100px" /> Xin vui lòng chờ trong giây lát sẽ có người hỗ trợ vấn đề của bạn. Cảm ơn bạn đã luôn tin tưởng Điện máy Đăng Nho 2 ạ. Chúc bạn một ngày tốt lành!');
+            });
         }
+    }
+
+    function addMessage(type, content) {
+        const messageElement = $("<div>").addClass(`message ${type}`).html(content);
+        chatBody.append(messageElement);
+        chatBody.scrollTop(chatBody.prop("scrollHeight"));
+    }
+
+    function showTypingEffect(callback) {
+        const typingElement = $("<div>")
+            .addClass("message reply typing")
+            .html('<span class="dot">.</span><span class="dot">.</span><span class="dot">.</span>');
+        chatBody.append(typingElement);
+        chatBody.scrollTop(chatBody.prop("scrollHeight"));
+
+        setTimeout(function () {
+            typingElement.remove();
+            callback();
+            chatBody.scrollTop(chatBody.prop("scrollHeight"));
+        }, 2000);
     }
 });
